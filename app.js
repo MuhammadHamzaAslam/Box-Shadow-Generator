31181d208c0a09f095b0c037a06cda330470ad8e
Hamza <153912992+MuhammadHamzaAslam@users.noreply.github.com>
2024-07-06 20:44:00 +0500

    done

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ function generate() {
     let bRadius = document.getElementById('b-radius').value
     let sRadius = document.getElementById('s-radius').value
     let sColor = document.getElementById('s-color').value
-    let sColorOpacity = document.getElementById('s-color-o').value
-    console.log(hShadow);
-    console.log(vShadow);
-    console.log(bRadius);
-    console.log(sRadius);
-    console.log(sColor);
-    console.log(sColorOpacity);
+    let insetCheck = document.getElementById('inset')
+    let box = document.getElementById('box')
+    let copyArea = document.getElementById('copyArea')
+
+    let shadow = `${insetCheck.checked ? 'inset' : ''} ${hShadow}px ${vShadow}px ${bRadius}px ${sRadius}px ${sColor}`
+    box.style.boxShadow = shadow
+
+    copyArea.innerText = `box-shadow:${shadow};`
 }
-// generate()
-document.getElementById('add').addEventListener('click',generate)
+function copyText() {
+    let copyText = document.getElementById("copyArea");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); 
+    document.execCommand("copy");
+    alert("Copied the text: " + copyText.value);
+}
